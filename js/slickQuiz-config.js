4a7e903047e5d4435271d0e5b432c3a6332653eb
Far'tash Rostamian <f_rost@encs.concordia.ca>
2017-11-09 15:39:58 -0500

    oo update 000.0001

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -4,8 +4,8 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Quiz!! How well do you know what's going on in your own backyard!?",
-        "main":    "<p>Think you're smart enough? Find out with this Quiz!</p>",
+        "name":    "How well do you know what’s going on in your city?",
+        "main":    "<p>Find out in this quiz!</p>",
         "results": "<h5>Learn More</h5><p>Check our [website]</p>",
         "level1":  "Jeopardy Ready",
         "level2":  "Jeopardy Contender",
@@ -15,41 +15,46 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "<div id='mainpic' ><img src='images/Proto01_01d.gif' ></div><div id='innerpic' style='display:none;'><img src='images/Proto01_01c.gif' ></div></br/>-Two criminals in Montreal get a call from their boss Eric and go to a Hospital where a worker is moving a cart with 15000$ in it, Do they?",
+            "q": "<div id='mainpic' ><img src='images/Proto01_01d.gif' ></div><div id='innerpic' style='display:none;'><img src='images/Proto01_01c.gif' ></div></br/>Two criminals in Montreal get a call from their boss &Eacute;ric with a job. They are to go to a hospital. There they find a worker pushing a cart with the hospital’s daily cash receipts on it, about $15,000. Do they?",
             
             "a": [
-                {"option": "Kill him & Rob him",      "correct": false},
-                {"option": "Rob him at gun point",     "correct": true},
+                {"option": "Kill the worker and rob him",      "correct": false},
+                {"option": "Rob the worker at gunpoint",     "correct": true},
+                {"option": "Beat the worker",      "correct": false},
                 {"option": "Walk away",      "correct": false},
                 //{"option": "23",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> You smartie!</p>",
-            "incorrect": "<p><span>Uhh no.</span> Not even close xD, Correct answer: Rob him at gun point</p>" // no comma here
+            "incorrect": "<p><span>Uhh no.</span> Not even close. <b>Correct answer:</b> Rob him at gunpoint</p>" // no comma here
             
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any  
             "q": "<div id='mainpic1' ><img src='images/Proto01_01c.gif' ></div><div id='innerpic1' style='display:none;'><img src='images/Proto01_01b.gif' ></div></br/>How do you think the criminals escaped?",
             "a": [
-                {"option": "Car",               "correct": false},
-                {"option": "Bicycles",   "correct": true},
-                {"option": "Running",               "correct": false},
+                {"option": "In a car",               "correct": false},
+                {"option": "By bicycle",   "correct": true},
+                {"option": "On foot",               "correct": false},
                 //{"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
             ],
             "select_any": true,
-            "correct": "<p><span>O Wow!</span> Someone is intuitive...</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options. Correct answer: Bicycles</p>" // no comma here
+            "correct": "<p><span>O wow!</span> Someone is intuitive...</p>",
+            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options. Correct answer: They got away on bikes.</p>" // no comma here
         },
         
         
         { // Question 5
-            "q": "<div id='mainpic2' ><img src='images/Proto01_01b.gif' ></div><div id='innerpic2' style='display:none;'><img src='images/Proto01_01a.gif' ></div></br/>Where do you think Eric is hiding?",
+            "q": "<div id='mainpic2' ><img src='images/Proto01_01b.gif' ></div><div id='innerpic2' style='display:none;'><img src='images/Proto01_01a.gif' ></div></br/>Where do you think &Eacute;ric is hiding?",
             "a": [
-                {"option": "Ontario",    "correct": false},
-                {"option": "Quebec",     "correct": false},
-                {"option": "The Moon!",     "correct": false} // no comma here
+                {"option": "Rural Ontario",    "correct": false},
+                {"option": "Vancouver",     "correct": false},
+                {"option": "Ottawa",     "correct": false}, // no comma here
+                {"option": "Rural Quebec",     "correct": false},
+                {"option": "Toronto",     "correct": false},
+                {"option": "Montreal",     "correct": false},
+                {"option": "The Moon!",     "correct": false},
             ],
             "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span><img src='Proto01_01a.gif'>WELLLL!</span> He's still totally on the loose! <br/> Thanks for playing, We will post a new Quiz very soon!</p>" // no comma here
+            "incorrect": "<p><span><img src='Proto01_01a.gif'>WELLLL!</span> He's still on the loose! <br/> Thanks for playing. We’ll post a new quiz very soon!</p>" // no comma here
         } // no comma here
     ]
 };
